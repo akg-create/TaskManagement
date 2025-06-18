c3a304f77d797e7293767e5a7f653e061f63704d
akg-create <abbie.grooms@live.com>
2025-06-18 18:34:43 -0400

    Update firestore-config.js

--- firestore-config.js
+++ firestore-config.js
@@ -1,24 +1,23 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBaQdUC88_6-I8t0C0B0TzDCPXOrZ7QJmM",
-  authDomain: "taskflow-f0704.firebaseapp.com",
-  projectId: "taskflow-f0704",
-  storageBucket: "taskflow-f0704.firebasestorage.app",
-  messagingSenderId: "415346813355",
-  appId: "1:415346813355:web:739918f2359c655eb40aee",
-  measurementId: "G-JEGFDSHSPY"
-};
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyBaQdUC88_6-I8t0C0B0TzDCPXOrZ7QJmM",
+    authDomain: "taskflow-f0704.firebaseapp.com",
+    projectId: "taskflow-f0704",
+    storageBucket: "taskflow-f0704.firebasestorage.app",
+    messagingSenderId: "415346813355",
+    appId: "1:415346813355:web:739918f2359c655eb40aee",
+    measurementId: "G-JEGFDSHSPY"
+  };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-
-///npm install firebase
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
